ec8ada9b3a6b2456a0b96d92bd96986fe34f19e2
thebra <jason.bradley@gmail.com>
2012-06-21 16:23:16 -0500

    Typo fix in example.php

--- example.php
+++ example.php
@@ -24,7 +24,7 @@
     $phpCli->printLine("Messsage with special color.", 'blue', 'light_gray');
     
     echo "\r\n";
-    echo $phpCli->printLine("This script is using ". $phpCli->getMemoryUsage() . " mb of memory.");
+    echo $phpCli->printLine("This script is using ". $phpCli->getMemoryUsage() . " of memory.");
     
     echo "\r\n";
     echo "Progress Bar Example:";
